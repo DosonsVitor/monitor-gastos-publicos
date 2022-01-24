35b9d11d41dd880595b4f43d8757452aafb0cbe6
Matheus-Ataide <96483920+Matheus-Ataide@users.noreply.github.com>
2022-01-24 17:09:55 -0300

    Compilando com o arquivo de texto
    Ainda não está mostrando ás 10 primeiras cidades, não colocamos em ordem descrescente

--- Exibir lista com os 10 municípios que mais receberam recurso do governo em 2021./dashboard_03.js
+++ Exibir lista com os 10 municípios que mais receberam recurso do governo em 2021./dashboard_03.js
@@ -1,1 +1,35 @@
+csv = "heading1,heading2,heading3,heading4,heading5\nvalue1_1,value2_1,value3_1,value4_1,value5_1\nvalue1_2,value2_2,value3_2,value4_2,value5_2",
 
+
+$(document).ready(function() {
+    $.ajax({
+        type: "POST",
+        url: "/echo/html/",
+        dataType: "text",
+        data: {
+            html: csv
+        },
+        success: function(data) {
+            processData(data);
+        }
+    });
+});
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+            var tarr = [];
+            for (var j=0; j<headers.length; j++) {
+                tarr.push(headers[j]+":"+data[j]);
+            }
+            lines.push(tarr);
+        }
+    }
+console.log(lines);
+}
